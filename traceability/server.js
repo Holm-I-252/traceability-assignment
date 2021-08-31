5c6352e938bd6d1ec34cb321742c8d4f85ba877e
Ian Holm <ianholm252@gmail.com>
2021-08-31 14:31:21 -0600

    trying to get rollbar to work

--- traceability/server.js
+++ traceability/server.js
@@ -13,6 +13,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.log("HTML loaded")
 })
 
 // app.get('/api/test', (req, res), () => {
